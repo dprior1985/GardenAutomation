9ffde7c8caf6b48f9afcbf02ed012599639ecf89
dprior1985 <dannyprior1985@gmail.com>
2016-05-23 22:07:59 +0100

    Change colours
    Different website colours based on day of week

--- MainCode/python/WebsiteSlim.py
+++ MainCode/python/WebsiteSlim.py
@@ -2,11 +2,13 @@ import datetime
 import MySQLdb
 import time, sys
 import numpy
+import calendar
 
 # Open database connection
 db = MySQLdb.connect("localhost","danny","danny123","MYGARDEN" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
+my_date = datetime.today()
 
 vLastTimeWaterData = "000";
 
@@ -131,9 +133,25 @@ t = datetime.datetime(2012, 2, 23, 0, 0)
 #if (float(Avgtest) < 14):
 #color = "Lightblue"
 #if (float(Avgtest) >= 14):
-color = "PeachPuff"
+
+if (calendar.day_name[my_date.weekday()] == "Monday")
+	color = "PaleGoldenrod"
+if (calendar.day_name[my_date.weekday()] == "Tuesday")
+	color = "Lavender"	
+if (calendar.day_name[my_date.weekday()] == "Wednesday")
+	color = "PaleTurquoise"
+if (calendar.day_name[my_date.weekday()] == "Thursday")
+	color = "Cornsilk"
+if (calendar.day_name[my_date.weekday()] == "Friday")
+	color = "BlanchedAlmond"	
+if (calendar.day_name[my_date.weekday()] == "Saturday")
+	color = "LightCoral"
+if (calendar.day_name[my_date.weekday()] == "Sunday")
+	color = "PeachPuff"
 	
- 
+	
+	
+	
 website="""
 <!DOCTYPE html>
 <html>
