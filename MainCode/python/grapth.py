3bd1a19bbcc503546baf5dbebcc071098082c1d3
dprior1985 <dannyprior1985@gmail.com>
2016-08-15 22:35:40 +0100

    Update grapth.py
    graph 1 only shows API temp

--- MainCode/python/grapth.py
+++ MainCode/python/grapth.py
@@ -29,8 +29,12 @@ font = {'family' : 'normal',
 
 matplotlib.rc('font', **font)
 
+x=0
+y=0
+xa=0
+ya=0
 
-cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 1' order by DateNow;" )
+cursor.execute("select cast(SaveData as decimal(16,2)),Datenow from ControlLog where ActionName = 'Weather API' and LogDescription = 'Temp C' order by DateNow;" )
 for row in cursor.fetchall():
 
 	x = (row[1])
@@ -38,12 +42,12 @@ for row in cursor.fetchall():
 	x1.append(x)
 	y1.append(y)
 
-cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 2' order by DateNow;" )
+#cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 2' order by DateNow;" )
 
-for row in cursor.fetchall():
+#for row in cursor.fetchall():
 
-	y = (row[0])
-	y2.append(y)
+#	y = (row[0])
+#	y2.append(y)
 
 
 
@@ -57,8 +61,8 @@ pyplot.ylabel('Temp C')
 pyplot.grid(b='on')
 
 
-pyplot.line = pyplot.plot(x1,y1, color='green',label='Temp Senor 1')
-pyplot.line = pyplot.plot(x1,y2, color='red',label='Temp Senor 2')
+pyplot.line = pyplot.plot(x1,y1, color='green',label='API Weather Station')
+#pyplot.line = pyplot.plot(x1,y2, color='red',label='Temp Senor 2')
 
 pyplot.legend(loc='best')
 
@@ -66,28 +70,29 @@ pyplot.savefig('/var/www/Graph1.png')
 pyplot.clf()
 
 
-cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 1' and subtime(now(), '24:00:00') <= DateNow order by DateNow; " )
-for row in cursor.fetchall():
-
-	xa = (row[1])
-	ya = (row[0])
-	x11.append(xa)
-	y11.append(ya)
-
-cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 2' and subtime(now(), '24:00:00') <= DateNow order by DateNow; " )
-
-for row in cursor.fetchall():
-
-	ya = (row[0])
-	y12.append(ya)
 
 
-pyplot.title('Temp Senors for last 24 hours')
-pyplot.legend(loc='best')
-pyplot.grid(b='on')
+#cursor.execute("select cast(Data as decimal(16,2)),Datenow from SenorLog where SensorName = 'temp sensor 2' and subtime(now(), '24:00:00') <= DateNow order by DateNow; " )
+#for row in cursor.fetchall():
 
-pyplot.line = pyplot.plot(x11,y11, color='green',label='Temp Senor 1')
-pyplot.line = pyplot.plot(x11,y12, color='red',label='Temp Senor 2')
+#	xa = (row[1])
+#	ya = (row[0])
+#	x11.append(xa)
+#	y11.append(ya)
+
+#cursor.execute("select Data,Datenow from SenorLog where SensorName = 'temp sensor 2' and subtime(now(), '24:00:00') <= DateNow order by DateNow; " )
+
+#for row in cursor.fetchall():
+
+#	ya = (row[0])
+#	y12.append(ya)
 
 
-pyplot.savefig('/var/www/Graph2.png')
+#pyplot.title('Temp Senors for last 24 hours')
+#pyplot.legend(loc='best')
+#pyplot.grid(b='on')
+
+#pyplot.line = pyplot.plot(x11,y11, color='green',label='Temp Senor 1')
+#pyplot.line = pyplot.plot(x11,y12, color='red',label='Inside Temp - Last 24 Hours')
+
+#pyplot.savefig('/var/www/Graph2.png')
