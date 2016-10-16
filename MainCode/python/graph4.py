453e24a9ad6642e56c3e0e928b0693300d847fc0
dprior1985 <dannyprior1985@gmail.com>
2016-10-16 21:40:30 +0100

    Update graph4.py
    add range value to chart

--- MainCode/python/graph4.py
+++ MainCode/python/graph4.py
@@ -17,6 +17,7 @@ x1 = []
 y1 = []
 y2 = []
 y3 = []
+y4 = []
 
 
 x11 = []
@@ -37,7 +38,7 @@ y=0
 xa=0
 ya=0
 yb=0
-
+yc=0
 cursor.execute("select max(cast(Data as decimal(16,2))),min(cast(Data as decimal(16,2))),avg(cast(Data as decimal(16,2))),cast(Datenow as date) Datenow from SenorLog where SensorName = 'temp sensor 2'  group by cast(Datenow as date)  order by cast(Datenow as date) ;" )
 for row in cursor.fetchall():
 
@@ -45,11 +46,13 @@ for row in cursor.fetchall():
 	y = (row[0])
 	ya = (row[1])
 	yb = (row[2])
+	yc = (y-ya)
 	x1.append(x)
 	y1.append(y)
 	y2.append(ya)
 	y3.append(yb)
-
+	y4.append(yc)
+	
 pyplot.title('Inside - MIN/MAX All time')
 pyplot.xlabel('Time')
 pyplot.ylabel('Temp C')
@@ -59,6 +62,7 @@ pyplot.grid(b='on')
 pyplot.line = pyplot.plot(x1,y1, color='red',label='Max Temp')
 pyplot.line = pyplot.plot(x1,y2, color='blue',label='Min Temp')
 pyplot.line = pyplot.plot(x1,y3, color='green',label='Avg Temp')
+pyplot.line = pyplot.plot(x1,y4, color='orange',label='Range Temp')
 
 pyplot.legend(loc='best')
 
