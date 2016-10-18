86cf6b57a856510c4e61477f9c8647020d18a62d
dprior1985 <dannyprior1985@gmail.com>
2016-10-18 22:52:08 +0100

    add if to only run water if less then minute 20

--- modules/openrelay.py
+++ modules/openrelay.py
@@ -14,15 +14,16 @@ def Run(sec):
 
 	try:
 		
-		if (sec > 0):
-			GPIO.setmode(GPIO.BOARD)
-			GPIO.setup(relay,GPIO.OUT)
-			print("Relay open")
-			print(datetime.now())
-			GPIO.output(relay,GPIO.LOW)
-			#os.system("sudo python /home/pi/Desktop/GardenAutomation/modules/segs.py")
-			time.sleep(sec)
-			#sec = sec - 1
+		if (now.minute <= 20):
+			if (sec > 0):
+				GPIO.setmode(GPIO.BOARD)
+				GPIO.setup(relay,GPIO.OUT)
+				print("Relay open")
+				print(datetime.now())
+				GPIO.output(relay,GPIO.LOW)
+				#os.system("sudo python /home/pi/Desktop/GardenAutomation/modules/segs.py")
+				time.sleep(sec)
+				#sec = sec - 1
 
 
 		GPIO.output(relay,GPIO.HIGH)
